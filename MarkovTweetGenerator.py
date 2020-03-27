0fa5576cb553b070f7ba70bf6ba2be170b7f91c9
Cephalon Wiki <43304356+CephalonWiki@users.noreply.github.com>
2020-03-27 06:18:38 -0400

    Changed from class to script, forward and backward prop, seed prop

--- MarkovTweetGenerator.py
+++ MarkovTweetGenerator.py
@@ -4,11 +4,36 @@ import random
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
-## INSPIRATION ##
-
-# Build off code in this article
+# INSPIRATION
+# Built off code in this article:
 # http://www.onthelambda.com/2014/02/20/how-to-fake-a-sophisticated-knowledge-of-wine-with-markov-chains/
+
+default_head = ("BEGINN", "NNOW")
+default_tail = ("ENND", "NNOW")
+
+
+def filter_tweet(tweet):
+    # initial text processing
+    flat_tweet = tweet
+    if flat_tweet.startswith("RT"):
+        flat_tweet = flat_tweet[flat_tweet.index(":")+1:]
+
+    # filtering
+    words = list(default_head)
+    for w in flat_tweet.split():
+        if w.startswith("http"):
+            continue
+        elif "http" in w:
+            words.append(w[:w.index("http")])
+        else:
+            words.append(w)
+    words += list(default_tail)
+
+    # exclude tweets that do not contain a full trigram
+    if len(words) < 6:
+        words = []
+    return words
+
 
 def generate_trigrams(words):
     if len(words) < 3:
@@ -17,79 +42,53 @@ def generate_trigrams(words):
         return list(zip(words[:-2], words[1:-1], words[2:]))
 
 
-class MarkovTweetGenerator:
+def generate_markov_chain(trigrams):
 
-    def __init__(self):
+    chain = defaultdict(list)
 
-        self.tweets = pd.read_csv("tweets.csv", parse_dates=['created_at'])
+    # Generate trigrams and create dictionary to compute probs
+    for trigram in trigrams:
+        chain[trigram[:2]].append(trigram[1:])
 
-        self.default_trigram = ("BEGIN", "NOW", "END")
-        self.tweets["filtered_text"] = self.tweets.text.apply(self.filter_tweet)
-        self.tweets["trigrams"] = self.tweets["filtered_text"].apply(generate_trigrams)
+    return chain
 
-        # keys are bigrams
-        self.chain = self.generate_markov_chain()
 
-        self.vectorizer = TfidfVectorizer()
-        self.tdMatrix = self.vectorizer.fit_transform(self.tweets.filtered_text.apply(lambda l: ' '.join(l)))
-        self.tdDataFrame = pd.SparseDataFrame(self.tdMatrix)
+def generate_tweet(chain, head=default_head, tail=default_tail):
 
-    def filter_tweet(self, tweet):
-        # initial text processing
-        flat_tweet = tweet
-        if flat_tweet.startswith("RT"):
-            flat_tweet = flat_tweet[flat_tweet.index(":")+1:]
+    prediction = []
 
-        # filtering
-        words = ["BEGIN", "NOW"]
-        for w in flat_tweet.split():
-            if w.startswith("http"):
-                continue
-            elif "http" in w:
-                words.append(w[:w.index("http")])
-            else:
-                words.append(w)
-        words.append("END")
+    current_bigram = random.choice(chain[head])
+    while current_bigram[1] != tail[0]:
+        prediction.append(current_bigram[1])
+        current_bigram = random.choice(chain[current_bigram])
 
-        # exclude tweets that do not contain a full trigram
-        if len(words) < 6:
-            words = list(self.default_trigram)
-        return words
+    return ' '.join(prediction)
 
-    def generate_markov_chain(self):
-        # reset chain on each call
-        chain = defaultdict(list)
 
-        # Loop through text in sources and generate trigrams
-        for gen in self.tweets.trigrams:
-            # Generate trigrams and create dictionary to compute probs
-            for trigram in gen:
-                if trigram == self.default_trigram:
-                    continue
-                else:
-                    chain[trigram[:2]].append(trigram[2])
+def generate_from(seed, chain):
+    seed_bigrams = []
+    for bigram in chain:
+        if seed in bigram[0] or seed in bigram[1]:
+            seed_bigrams.append(bigram)
+    seed_bigram = random.choice(seed_bigrams)
 
-        return chain
+    prediction = []
 
-    def generate_tweet(self):
-        prediction = []
+    forward_seed = generate_tweet(forward_chain, head=seed_bigram).split()
+    reverse_seed = generate_tweet(reverse_chain, head=seed_bigram[::-1], tail=default_head[::-1]).split()[::-1]
 
-        # Random/Generic start point
-        sword1 = "BEGIN"
-        sword2 = "NOW"
+    prediction = reverse_seed + list(seed_bigram) + forward_seed
 
-        #     # Choose starting tweet containing seed
-        #     tweet_seed = random.choice(filter(lambda s:  seed in s, sources))
-        #
-        #     # Take starting words from tweet_seed
-        #     sword1 = tweet_seed = [2]
-        #     sword2 = tweet_seed = [3]
+    return " ".join(prediction)
 
-        while True:
-            sword1, sword2 = sword2, random.choice(self.chain[(sword1, sword2)])
-            if sword2 == "END":
-                break
-            prediction.append(sword2)
 
-        return ' '.join(prediction)
+tweets = pd.read_csv("tweets.csv", parse_dates=['created_at'])
 
+tweets["filtered_text"] = tweets.text.apply(filter_tweet)
+tweets["trigrams"] = tweets["filtered_text"].apply(generate_trigrams)
+forward_chain = generate_markov_chain(tweets["trigrams"].sum())
+reverse_chain = generate_markov_chain(tweets["filtered_text"].apply(lambda l: l[::-1]).apply(generate_trigrams).sum())
+
+vectorizer = TfidfVectorizer(analyzer='word')
+tfMatrix = vectorizer.fit_transform(tweets.filtered_text.apply(lambda l: ' '.join(l)))
+tfDataFrame = pd.SparseDataFrame(tfMatrix)
